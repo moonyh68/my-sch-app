e5424464e53e07daa2f67131dafbe71d3a34bec6
moonyh68 <moonyh68@gmail.com>
2026-08-07 13:57:10 +0900

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- sch.py
+++ sch.py
@@ -73,7 +73,7 @@ st.markdown("""
         background-color: transparent !important;
         border: none !important;
         padding: 0px !important;
-        margin-top: -6px !important; /* 음수 마진으로 버튼 밑으로 바짝 끌어올림 */
+        margin-top: -4px !important; /* 음수 마진으로 버튼 밑으로 바짝 끌어올림 */
     }
 
     .task-item {
